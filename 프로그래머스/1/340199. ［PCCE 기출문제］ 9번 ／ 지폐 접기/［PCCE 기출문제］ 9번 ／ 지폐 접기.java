d6daf99b9388c67d0b03e5e615cef807760b5d90
MinjiKim-123 <mmmmin0729@gmail.com>
2024-12-03 00:32:12 +0900

    Update ［PCCE 기출문제］ 9번 ／ 지폐 접기.java

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,6 +1,28 @@
 class Solution {
     public int solution(int[] wallet, int[] bill) {
-        int answer = 0;
-        return answer;
+        int billSize1 = bill[0];
+		int billSize2 = bill[1];
+		int walletSize1 = wallet[0];
+		int walletSize2 = wallet[1];
+		
+		int answer = 0;
+		int maxLoop = 10;//무한 루프 실행을 막기 위해 max 설정
+		
+		while(true && answer <= maxLoop) {
+			
+			if(billSize1 < walletSize1 && billSize2 < walletSize2 
+					|| billSize1 < walletSize2 && billSize2 < walletSize1) 
+				break;
+			
+			
+			if(billSize1 > billSize2)
+				billSize1 = billSize1 / 2;
+			else 
+				billSize2 = billSize2 / 2;
+				
+			answer++;
+		}
+		
+		return answer;
     }
 }
