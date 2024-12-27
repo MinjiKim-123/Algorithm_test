84d15a8658a021694d5debe2254361ca7a197c47
MinjiKim-123 <mmmmin0729@gmail.com>
2024-12-28 00:23:47 +0900

    Update 자릿수 더하기.java
    나중에 참고를 위한 다른 풀이 방법 추가

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -11,4 +11,17 @@ public class Solution {
 
         return answer;
     }
+
+	
+   public int solution2(int n) {
+        int answer = 0;
+
+	//찾아보니 나눗셈을 이용해서 풀이를 진행하는 방법이 있는데 이 방법이 성능 및 가독성 면에서 훨씬 나아보인다.
+         while(n > 0){
+            answer += n % 10;
+            n = n/10;
+        }
+	   
+        return answer;
+    }
 }
