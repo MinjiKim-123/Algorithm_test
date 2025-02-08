0844c8da891a7abc840a70de7985e57c1dfbf539
MinjiKim-123 <mmmmin0729@gmail.com>
2025-02-08 22:27:02 +0900

    solution을 두개로 수정

--- 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.java
@@ -1,8 +1,30 @@
 import java.util.*;
 
 class Solution {
-   
-    public int[] solution(int[] array, int divisor) {
+
+    //방법1
+    public int[] solution2(int[] array, int divisor) {
+        List<Integer> list = new ArrayList<>();
+        for (int num : arr) {
+             if (num % divisor == 0) 
+				list.add(num);
+        }
+
+        if (list.size() == 0)
+            list.add(-1);
+	
+		Collections.sort(list);
+		
+        int[] answer = new int[list.size()];
+        for(int i=0; i<list.size();i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+
+    // 방법2
+    //이렇게 stream의 filter를 사용하는 방법도 괜찮을 듯..??
+    public int[] solution2(int[] array, int divisor) {
         int [] answer = Arrays.stream(array).filter(num -> num % divisor ==0).sorted().toArray();
 		return answer.length == 0 ? new int[] {-1} : answer;
     }
